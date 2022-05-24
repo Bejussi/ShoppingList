ee53131e65a56125981914a28bd0559ba07264b2
Alexandra <pindooqy@gmail.com>
2022-05-24 21:22:10 +0300

    Create domain layer, business logic of app

---
+++ app/src/main/java/com/bejussi/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,11 @@
+package com.bejussi.shoppinglist.domain
+
+interface ShopListRepository {
+
+    fun addShopItem(shopItem: ShopItem)
+    fun deleteShopItem(shopItemId: Int)
+    fun editShopItem(shopItem: ShopItem)
+    fun getShopItem(shopItemId: Int): ShopItem
+    fun getShopList(): List<ShopItem>
+
+}
